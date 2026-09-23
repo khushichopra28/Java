577d66f937466ce349eab1e0a9778afa54ecdf9c
Khushi Chopra <khushi.mahaveer.chopra@gmail.com>
2026-09-23 09:50:29 +0530

    4 methods

--- method.java
+++ method.java
@@ -73,12 +73,34 @@
 //     }
 // }
 
+// public class method{
+//     public static String main()
+// {
+//     return "hello";
+// }public static void main(String[]args){
+//     System.out.println(main());
+// }
+// }
 public class method{
-    public static String main()
-{
-    return "hello";
-}public static void main(String[]args){
-    System.out.println(main());
+    public static int add(int a,int b){
+        return a+b;
+    }
+    public static int sub(int a, int b){
+        return a-b;
 
+    }
+    public static int mult(int a, int b){
+        return a*b;
+    }
+    public static int div(int a, int b){
+        return a/b;
+    }
+    public static void main(String[]args){
+        System.out.println(add(10,20));
+        System.out.println(sub(10,20));
+        System.out.println(mult(10,20));    
+        System.out.println(div(10,20));
+    }
 }
-}
+
+
